1f5213d3dc779ed11e1128f1a4600709baab34b2
nandhh <nandhh@gmail.com>
2026-02-06 17:04:24 +0800

    v0.3.0

--- src/primitives/indexer.cpp
+++ src/primitives/indexer.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 The Fractal Bitcoin developers
+// Copyright (c) 2026 The Fractal Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
